b2913490b945b06aac8ac814ac31d3e4b49cd865
hyell2222 <parkhyell2222@gmail.com>
2026-07-27 08:48:03 +0900

    Fix : delete legacy

--- submission/source/eslint.config.mjs
+++ submission/source/eslint.config.mjs
@@ -1,15 +1,10 @@
 import { defineConfig, globalIgnores } from "eslint/config";
-import nextVitals from "eslint-config-next/core-web-vitals";
-import nextTs from "eslint-config-next/typescript";
 
 const eslintConfig = defineConfig([
-  ...nextVitals,
-  ...nextTs,
-  // Override default ignores of eslint-config-next.
   globalIgnores([
     "dist/**",
-    "supabase/functions/**",
-    "next-env.d.ts",
+    "submission/**",
+    "node_modules/**",
   ]),
 ]);
 
